3be8ecfd9353195953cabeb0b97eb6952082c92f
Jorg Thuijls <jorg.thuijls@gmail.com>
2014-11-20 12:49:54 +1000

    moved to a new laptop

---
+++ build.js
@@ -0,0 +1,8 @@
+({
+    mainConfigFile : "public/js/main.js",
+    baseUrl : "public/js",
+    name: "main",
+    out: "public/dist/main.min.js",
+    removeCombined: true,
+    findNestedDependencies: true
+})
